9d4d0c16a6eb67e917391b4785baff07c69e811b
Zilan1 <79017444+Zilan1@users.noreply.github.com>
2022-03-26 15:35:27 +0300

    sum

---
+++ proje57.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+   int s1,s2,t,f,c,b;
+   
+   printf("Birinci sayiyi giriniz: ");
+   scanf("%d",&s1);
+   
+   printf("IKinci Sayiyi Giriniz: ");
+   scanf("%d",&s2);
+   
+   t=s1+s2;
+   f=s1-s2;
+   c=s1*s2;
+   b=s1/s2;
+   
+   printf("Toplam: %d\n",t);
+   printf("Fark: %d\n",f);
+   printf("Carpim: %d\n",c);
+   printf("Bolum: %d\n",b);
+   
+   
+   
+   
+   
+   
+    //char Ad[10],Soyad[10],Yas[2],Sehir[15],Meslek[15];
+
+    //printf("Adiniz: ");
+    //scanf("%s",Ad);
+
+    //printf("Soyadiniz: ");
+    //scanf("%s",Soyad);
+    
+    //printf("Yasiniz: ");
+    //scanf("%s",Yas);
+    
+    //printf("Yasadiginiz Sehir: ");
+    //scanf("%s",Sehir);
+
+    //printf("Mesleginiz: ");
+    //scanf("%s",Meslek);
+
+    //printf("\n");
+
+    //printf("Adi: %s\n",Ad);
+    //printf("Soyadi: %s\n",Soyad);
+    //printf("Yasi: %s\n",Yas);
+    //printf("Sehri: %s\n",Sehir);
+    //vprintf("Meslegi: %s\n",Meslek);
+
+
+
+	//char kitapadi[20]="Hayvan Ciftligi";
+    //char kitapyazari[15]="Geoge Orwell";
+    //char kitapsayfa[3]="112";
+    
+    //printf("%s\n",kitapadi);
+    //printf("%s\n",kitapyazari);
+    //printf("%s\n",kitapsayfa);
+    
+    //printf("~~~~~Kitap Tanitim~~~~~\n\n\n");
+    //printf("Kitap Adi: %s\n",kitapadi);
+    //printf("Kitap Yazari: %s\n",kitapyazari);
+    //printf("Sayfa Sayisi: %s\n",kitapsayfa);
+    
+    
+    
+	//printf("Merhaba Dunya");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
